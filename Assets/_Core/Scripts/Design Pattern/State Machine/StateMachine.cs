3c14ebb6a0565d9151e903bf4c4dc76c44121e45
Logan <pix.59700@gmail.com>
2026-01-09 13:41:29 +0100

    Add state machine pattern

---
+++ Assets/_Core/Scripts/Design Pattern/State Machine/StateMachine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace ColoredGess.StateMachine
+{
+    public abstract class StateMachine<T> : MonoBehaviour where T : struct
+    {
+        protected T _data;
+        
+        private IState<T> _currentState;
+
+        protected virtual void Update()
+        {
+            IState<T> nextState = _currentState?.Update(_data);
+            if (nextState != null)
+                TransitionTo(nextState);
+        }
+
+        protected void TransitionTo(IState<T> nextState)
+        {
+            _currentState?.Exit(_data);
+            _currentState = nextState;
+            _currentState?.Enter(_data);
+        }
+    }
+}
