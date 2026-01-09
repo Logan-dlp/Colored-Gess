f5aca32f618cd9cb4178db2f7b42ec0e93849e3e
Logan <pix.59700@gmail.com>
2026-01-09 13:43:20 +0100

    Color value

---
+++ Assets/_Core/Scripts/Colors/ColorsType.cs
@@ -0,0 +1,14 @@
+﻿namespace ColoredGess.Colors
+{
+    public enum ColorsType
+    {
+        UNDEFINED,
+        Color_A,
+        Color_B,
+        Color_C,
+        Color_D,
+        Color_E,
+        Color_F,
+        Color_G,
+    }
+}
