f941cdec93d6ba234e89d9953915dd1ca5e57a16
Logan <pix.59700@gmail.com>
2026-01-09 13:47:59 +0100

    Make complete sate machine for game

---
+++ Assets/_Core/Scripts/Players/StateMachine/PlayerStateMachine.cs
@@ -0,0 +1,19 @@
+﻿namespace ColoredGess.Players
+{
+    using StateMachine;
+    using Colors;
+    
+    public class PlayerStateMachine : StateMachine<PlayerStateData>
+    {
+        protected void Awake()
+        {
+            _data = new PlayerStateData
+            {
+                CurrentLineIndex = 0,
+                ColorToGessArray = new ColorsType[ColoredGessParameter.MaxColorsToGess],
+            };
+            
+            TransitionTo(new AutoChooseColor());
+        }
+    }
+}
