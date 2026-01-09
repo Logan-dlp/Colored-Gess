9509ffba958bc5dd2a007fd00438dd017e08a6c3
Logan <pix.59700@gmail.com>
2026-01-09 13:46:05 +0100

    Change line and index

---
+++ Assets/_Core/Scripts/Players/Inputs/Index/GessLineHandler.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using ColoredGess.Colors;
+
+namespace ColoredGess.Players
+{
+    public class GessLineHandler : MonoBehaviour
+    {
+        public Action<int> OnIndexChange;
+
+        [SerializeField] private GameObject _cursorObject;
+        [SerializeField] private GameObject[] _lineArray;
+        
+        public void EnableLine(int index)
+        {
+            int currentIndex = 0;
+            
+            foreach (Transform child in _lineArray[index].transform)
+            {
+                var indexController = child.gameObject.AddComponent<IndexController>();
+                indexController.SetUp(currentIndex);
+                indexController.OnIndexChange += (indexChange, position) =>
+                {
+                    OnIndexChange?.Invoke(indexChange);
+                    SetUpCursorPosition(position);
+                };
+
+                if (currentIndex == 0)
+                {
+                    SetUpCursorPosition(child.position);
+                }
+                
+                currentIndex++;
+            }
+        }
+
+        public void DisableLine(int index)
+        {
+            foreach (Transform child in _lineArray[index].transform)
+            {
+                if (child.TryGetComponent(out IndexController indexController))
+                {
+                    indexController.OnIndexChange -= (indexChange, position) =>
+                    {
+                        OnIndexChange?.Invoke(indexChange);
+                        SetUpCursorPosition(position);
+                    };
+                    Destroy(indexController);
+                }
+            }
+        }
+
+        public void SetUpColorAt(int line, int index, ColorsType color)
+        {
+            _lineArray[line].transform.GetChild(index).gameObject.GetComponent<SpriteRenderer>().color = color.Color();
+        }
+
+        public void SetUpCursorPosition(int line, int index)
+        {
+            _cursorObject.transform.position = _lineArray[line].transform.GetChild(index).transform.position;
+        }
+
+        private void SetUpCursorPosition(Vector3 position)
+        {
+            _cursorObject.transform.position = position;
+        }
+    }
+}
