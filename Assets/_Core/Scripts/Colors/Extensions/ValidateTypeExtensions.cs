6ffdd4b1f38317612020a08b92a070a400a87d0a
Logan <pix.59700@gmail.com>
2026-01-17 00:13:04 +0100

    Add validate type

---
+++ Assets/_Core/Scripts/Colors/Extensions/ValidateTypeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ColoredGess.Colors
+{
+    public static class ValidateTypeExtensions
+    {
+        public static Color Color(this ValidateType validateType)
+        {
+            switch (validateType)
+            {
+                default:
+                    return UnityEngine.Color.magenta;
+                
+                case ValidateType.RIGHT_PLACE:
+                    return new Color(.004f, .4f, .188f, 1f);
+                case  ValidateType.NOT_RIGHT_PLACE:
+                    return new Color(.537f, .294f, .039f, 1f);
+                case ValidateType.NO_PLACE:
+                    return new Color(.623f, .027f, .07f, 1f);
+            }
+        }
+    }
+}
---
+++ Assets/_Core/Scripts/Colors/ValidateType.cs
@@ -0,0 +1,10 @@
+﻿namespace ColoredGess.Colors
+{
+    public enum ValidateType
+    {
+        UNDEFINED,
+        RIGHT_PLACE,
+        NOT_RIGHT_PLACE,
+        NO_PLACE,
+    }
+}
