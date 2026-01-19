8847627f1adabb7892356e0957d450f9f3e42ae2
Logan <pix.59700@gmail.com>
2026-01-19 16:46:11 +0100

    add persistant in MonoSingleton

---
+++ Assets/_Core/Scripts/Design Pattern/Singletons/PersistentMonoSingleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ColoredGess.Singletons
+{
+    public abstract class PersistentMonoSingleton<T> : MonoSingleton<T> where T : MonoSingleton<T>
+    {
+        [SerializeField] private bool _isUnparentOnAwake = true;
+
+        protected override void OnInitializing()
+        {
+            if (_isUnparentOnAwake)
+                transform.SetParent(null);
+            
+            base.OnInitializing();
+            if (Application.isPlaying)
+                DontDestroyOnLoad(gameObject);
+        }
+    }
+}
